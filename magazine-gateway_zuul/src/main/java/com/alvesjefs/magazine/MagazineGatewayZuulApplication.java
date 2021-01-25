f2a8205c1f0cec3be058e151845223ce218acb9a
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-01-25 08:30:31 -0300

    magazine-gateway_zuul

---
+++ magazine-gateway_zuul/src/main/java/com/alvesjefs/magazine/MagazineGatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.alvesjefs.magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class MagazineGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineGatewayZuulApplication.class, args);
+	}
+
+}
